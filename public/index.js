36610229726ace736fc050f575fe23a954096dcf
unknown <vladiconcept34@gmail.com>
2025-05-04 16:20:10 +0300

    Added PWA

--- public/index.js
+++ public/index.js
@@ -4,27 +4,14 @@ const startButton = document.getElementById("startBtn");
 const manualButton = document.getElementById("manualBtn");
 const quizButton = document.getElementById("quizBtn");
 const infoButton = document.getElementById("infoBtn");
-const addBtn = document.getElementById('add-to-home-screen');
 
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-    e.preventDefault();
-    deferredPrompt = e;
+if ('serviceWorker' in navigator) {
     
-    addBtn.style.display = 'block';
-
-    addBtn.addEventListener('click', () => {
-        deferredPrompt.prompt();
-        
-        deferredPrompt.userChoice.then((choiceResult) => {
-            console.log(choiceResult.outcome);
-            if (choiceResult.outcome === 'accepted') {
-                console.log('User accepted the A2HS prompt');
-            } else {
-                console.log('User dismissed the A2HS prompt');
-            }
-            deferredPrompt = null;
-        });
+    navigator.serviceWorker.register("service-worker.js").then(registration => {
+        console.log("SW registered!");
+        console.log(registration);
+    }).catch(error => {
+        console.log("SW registration failed!");
+        console.log(error);
     });
-});
+}
