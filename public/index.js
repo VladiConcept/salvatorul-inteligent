3ce31482469e5a2c98dc2c93cf292caec488a6a7
unknown <vladiconcept34@gmail.com>
2025-05-04 16:27:38 +0300

    Added PWA prompt

--- public/index.js
+++ public/index.js
@@ -5,8 +5,30 @@ const manualButton = document.getElementById("manualBtn");
 const quizButton = document.getElementById("quizBtn");
 const infoButton = document.getElementById("infoBtn");
 
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  
+  const installBtn = document.getElementById('install-btn');
+  installBtn.style.display = 'block';
+
+  installBtn.addEventListener('click', () => {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User installed the app');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+      deferredPrompt = null;
+    });
+  });
+});
+
+
 if ('serviceWorker' in navigator) {
-    
     navigator.serviceWorker.register("service-worker.js").then(registration => {
         console.log("SW registered!");
         console.log(registration);
